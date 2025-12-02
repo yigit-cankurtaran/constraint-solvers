075d804bfa7519ecd5fe9c0d4f0f736fe0e20d1a
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-12-02 20:44:15 +0300

    found about what add really does, will create new file

--- profit_if.py
+++ profit_if.py
@@ -14,3 +14,10 @@ s.add(income == 10, loss == 7)
 s.add(profit != 3)  # trying to contradict expected val
 
 print(s.check())  # returns unsat
+
+s.add(profit >= 3)
+print(s.check())  # returns unsat as well
+# the issue is, add ADDS constraints, doesn't write over the previous one
+# we are adding another constraint on top of a wrong constraint.
+# the first constraint will always be wrong, therefore both of our results
+# will return "unsat"
